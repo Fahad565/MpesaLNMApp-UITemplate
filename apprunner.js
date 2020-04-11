b2db4231319ee5a08448209e42b409c8da7d3013
Daniel  Ng'ang'a <junem3ta@gmail.com>
2020-04-11 05:45:25 +0300

    Update apprunner.js

--- apprunner.js
+++ apprunner.js
@@ -10,28 +10,29 @@ const Utils = require("./Utils.js");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+/*Allow cross origin requests*/
 app.use(function(req,res,next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-/* Project setup */
-/** 3) Serve main HTML file */
+/* AppUI Setup */
+/** 1. Serve main HTML file */
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-/** 4) Serve static assets  */
+/** 2. Serve static assets  */
 app.use(express.static(__dirname + "/public"));
-
 /* Preserve code above this comment */
 
-/* Core app procedures */
+/*Custom code here*/
 
+/*Preserve code below this comment*/
 
 /*Listener*/
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
