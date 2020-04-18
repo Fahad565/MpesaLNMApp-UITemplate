e67a44872b7e7c43a49a8f2cb5b07aa3d61e11ec
Daniel  Ng'ang'a <junem3ta@gmail.com>
2020-04-19 01:57:46 +0300

    Update apprunner.js

--- apprunner.js
+++ apprunner.js
@@ -26,12 +26,78 @@ app.get("/", function(req, res) {
 
 /** 2. Serve static assets  */
 app.use(express.static(__dirname + "/public"));
-/* Preserve code above this comment */
 
+/* @GlobalParam localCache -- [{"status": "", "requestId":"", "resultCode": ""}]
+  ** For interfacing with DOM: AjaxCall->Endpoint->ReturnLocalCache where RequestId
+      1. Prevent DOM From invoking dbCalls Directly
+      2. Avoid Manipulating DOM Directly from Node.js Server.
+      3. Use cache entries to update MongoDB Collections.
+*/
+let localCache = [];
+/*
+  @HelperFn updateLocalCache(requestId, status)
+  ** Update LocalCache For DOM References & MongoDB Collection updates. Used to update localcache entries with
+  the callBackURL results.
+  * A corresponding entry is added to a transactionDB for each Object updated in this localCache.
+  * For efficiency, the Object referring to a transaction is deleted from the LocalCache at the end of the TransactionCycle.
+
+  *The localcache has an object entry - {"requestId": "", "status": ""}  - for each transaction that ...
+  * ...is updated when the /process endpoint is invoked.
+  *Update the localCache entry for a specific json object and set status to either Completed | Cancelled | Failed
+*/
+function updateLocalCache(requestID, status) {
+  for(let entry of localCache) {
+    if(entry.requestID == requestID) {
+      entry.status = status;
+      entry.callBackStatus = true;
+      console.log(`LocalCache Update ${JSON.stringify(localCache)} @Test2[From CallBackURL]`);
+      break;
+    }
+  }
+}
+/*The /process Transaction is to be finished in ~54s.
+ **This fn will update the db collection for transactions with unresolved callBacks. We can call this fn 
+   10s after transaction Completion [60s] making the flushAndUpdate independent of endpoint traffic, rather 
+   than the original idea of having the flushAndUpdateDB() fn called for each transaction.
+ **With increased user traffic, the localCache can grow to a considerable size in a short span of time. By including 
+   the flushAndUpdateDB() fn, we empty the untracked transactions in our localCache into our db. 
+   This prevents the localCache from building up to undesirable sizes.
+ */
+function flushAndUpdate() {
+  if(localCache.length>0) {
+    localCache.filter((entry)=> {
+      if(!(entry.callBackStatus)) {
+        let data = entry;
+        data.resultCode = "Unkown";
+        data.status = "Unresolved";
+        data.resultDesc = "[Error] Unresolved Callback";
+
+        mongo.connect(process.env.MONGO_URI, {useNewUrlParser: true}, (err,db)=>{
+          if(err) {
+            console.log("DBConnectionERR " + err);
+            process.exit(0);
+          }
+          let db0 = db.db(process.env.DB);
+          let collection = db0.collection(process.env.COLLECTION);
+          collection.insertOne(data, (err, result) => {
+            if(err) {
+              console.log(`@Flush&UpdateInsertErr: ${err}`);
+              process.exit(0);
+            }
+          });
+        });
+      };
+      //The filter fn Deletes entries where callBackStatus == false;
+      return entry.callBackStatus == true;
+    });
+  }
+}
+/* Preserve code above this comment */
 /*Custom code here*/
 
-/*Preserve code below this comment*/
 
+
+/*Preserve code below this comment*/
 /*Listener*/
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
