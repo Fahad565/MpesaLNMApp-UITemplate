3e655e20bac3019eef1e2e2faadd3248b1e088c7
Daniel  Ng'ang'a <junem3ta@gmail.com>
2020-04-19 09:56:53 +0300

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -1,5 +1,10 @@
 const https = require("https");
 
 module.exports = {
-  
+  /*
+    testExportFn: function() {
+      console.log("Modular Fns Test");
+    }
+  */
+  /* Delimit functions within the module.exports section using a comma*/
 };
